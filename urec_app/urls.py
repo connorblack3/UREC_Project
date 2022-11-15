d704e2acd21185c92a109fa07563f5f52493b026
mcPython04 <chen1911893611@gmail.com>
2022-11-14 21:45:01 -0600

    added dummy pages for buttons

--- urec_app/urls.py
+++ urec_app/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accident/', views.accident, name='accident'),
+    path('count/', views.count, name='count'),
+    path('erp/', views.erp, name='erp'),
+    path('form/', views.form, name='form'),
+    path('incident/', views.incident, name='incident'),
+    path('sop/', views.sop, name='sop'),
+    path('survey/', views.survey, name='survey'),
+    path('task/', views.task, name='task'),
+
 ]
