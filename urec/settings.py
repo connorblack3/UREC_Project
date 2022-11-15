d16333c50e9225690607855169677139c4362985
mcPython04 <chen1911893611@gmail.com>
2022-11-14 19:45:25 -0600

    added buttons on home button; created views

--- urec/settings.py
+++ urec/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'urec_app.apps.UrecAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -79,23 +80,23 @@ WSGI_APPLICATION = 'urec.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ['RDS_DB_NAME'],
+#         'USER': os.environ['RDS_USERNAME'],
+#         'PASSWORD': os.environ['RDS_PASSWORD'],
+#         'HOST': os.environ['RDS_HOSTNAME'],
+#         'PORT': os.environ['RDS_PORT'],
+#     }
+# }
 
 
 # Password validation
--- urec_app/views.py
+++ urec_app/views.py
@@ -4,4 +4,36 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the home page!")
+    return render(request, 'urec_app/home.html')
+
+
+def accident(request):
+    return render(request, 'urec_app/accident.html')
+
+
+def count(request):
+    return render(request, 'urec_app/count.html')
+
+
+def erp(request):
+    return render(request, 'urec_app/erp.html')
+
+
+def form(request):
+    return render(request, 'urec_app/form.html')
+
+
+def incident(request):
+    return render(request, 'urec_app/incident.html')
+
+
+def sop(request):
+    return render(request, 'urec_app/sop.html')
+
+
+def task(request):
+    return render(request, 'urec_app/task.html')
+
+
+def survey(request):
+    return render(request, 'urec_app/survey.html')
